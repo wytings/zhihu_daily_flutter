df063b41e9a3ae8d0533c6918c45f776455691ef
Rex Wei <wytings@gmail.com>
2018-07-27 10:30:16 +0800

    separate mix code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:zhihu_daily_flutter/main.dart';
+import 'package:zhihu_daily_flutter/page/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
